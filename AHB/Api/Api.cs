18f18f591efd99e2dfd51d0138330838745167f4
Bakhan-eng <baxanb15@yahoo.com>
2025-10-06 01:01:56 +0200

    Setup full stack project structure: Backend API with SQLite DB and Blazor AHB frontend

--- AHB/Api/Api.cs
+++ AHB/Api/Api.cs
@@ -2,15 +2,23 @@ namespace AH.Api;
 
 public class Api
 {
-        public HttpClient _httpClient;
+    public HttpClient _httpClient;
 
-        public Api(HttpClient httpClient)
-        {
-                _httpClient = httpClient;
-        }
+    public Api(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
 
-        public async Task<List<Article>> GetArticle()
-        {
-                return await _httpClient.GetFromJsonAsync<List<Article>>("http://localhost:5262/products");
-        }
+    public async Task<List<Article>> GetArticle()
+    {
+        return await _httpClient.GetFromJsonAsync<List<Article>>("http://localhost:5262/products");
+    }
+
+
+     /// Lägg till en produkt
+    public async Task AddProductAsync(Article article)
+    {
+        await _httpClient.PostAsJsonAsync("https://localhost:7064/products", article);
+    }
 }
+        
